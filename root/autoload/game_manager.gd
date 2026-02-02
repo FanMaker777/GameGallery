62dc37172a8028f2c4d1bd583527fab1c412d1cd
FanMaker777 <myosotis465@gmail.com>
2026-02-02 21:02:50 +0900

    ビジュアルノベルゲーム(プロトタイプ)を実装
    ・ビジュアルノベルゲームのプロトタイプを実装
    ・ゲーム選択画面から、ビジュアルノベルゲームへの遷移を実装

--- root/autoload/game_manager.gd
+++ root/autoload/game_manager.gd
@@ -1,21 +1,21 @@
 ## 【Autoload】メニューやレベルのシーン切り替えを担当する
 extends Node
 
-# メインメニューとレベルシーンをプリロード
+# メインメニューをプリロード
 const MAIN_MENU_SCENE: PackedScene = preload("uid://byjaiv21t5df7")
 
 func _ready() -> void:
 	Log.current_log_level = Log.LogLevel.DEBUG
 
-# メインメニューシーンをロードする関数
-func load_main_scene() -> void:
-	Log.debug("func load_main_scene")
+# 引数のシーンを、遷移エフェクト付きでロードする関数
+func load_scene_with_transition(load_to_scene:PackedScene) -> void:
+	Log.info("func load_scene_with_transition", load_to_scene)
 	# 画面をフェードアウト
 	TransitionEffect.fade_out()
 	# フェードアウト終了後
 	TransitionEffect.finished_fade_out.connect(func() -> void:
 		# メインメニューに遷移
-		get_tree().change_scene_to_packed(MAIN_MENU_SCENE)
+		get_tree().change_scene_to_packed(load_to_scene)
 		# メインメニューのシーン展開完了まで待機
 		await get_tree().scene_changed
 		# 画面をフェードイン
