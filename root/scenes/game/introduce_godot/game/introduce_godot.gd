ad89423c78728f98418b2f8349da4b9a21c985ff
FanMaker777 <myosotis465@gmail.com>
2026-02-07 00:15:38 +0900

    会話中の演出を追加

--- root/scenes/game/introduce_godot/game/introduce_godot.gd
+++ root/scenes/game/introduce_godot/game/introduce_godot.gd
@@ -5,9 +5,40 @@ extends Control
 ## Audio player that plays voice sounds while text is being written
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 @onready var pause_screen: Control = %PauseScreen
+@onready var _explanation_panel_container: PanelContainer = %ExplanationPanelContainer
+@onready var _explanation_panel_container_2: PanelContainer = %ExplanationPanelContainer2
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
-	
+	# 説明用の画像を非表示に設定
+	_explanation_panel_container.visible = false
+	_explanation_panel_container_2.visible = false
+	# アドオン「Dialogic」から受け取るシグナルを接続
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	# アドオン「Dialogic」のタイムラインを開始
 	Dialogic.start("introduce_godot")
+
+## アドオン「Dialogic」からのシグナル受信時メソッド
+## argument:String=Dialogicでシグナル発信時に設定した引数
+func _on_dialogic_signal(argument:String) -> void:
+	Log.debug("Dialogicのシグナルを検知", argument)
+	
+	match argument:
+		"show_explanation":
+			Log.debug("show_explanation")
+			_explanation_panel_container.visible = true
+			var tween:Tween = create_tween()
+			tween.tween_property(_explanation_panel_container, "modulate:a", 1.0, 0.5).from(0.0)
+		"show_explanation2":
+			Log.debug("show_explanation2")
+			_explanation_panel_container_2.visible = true
+			var tween:Tween = create_tween()
+			tween.tween_property(_explanation_panel_container_2, "modulate:a", 1.0, 0.5).from(0.0)
+		"hide_explanation":
+			Log.debug("hide_explanation")
+			_explanation_panel_container.visible = false
+		"hide_explanation2":
+			Log.debug("hide_explanation2")
+			_explanation_panel_container_2.visible = false
+	
