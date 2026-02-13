d1284af0d6448b1c8ee0e365c77cc0c69e355f50
FanMaker777 <myosotis465@gmail.com>
2026-02-13 14:23:01 +0900

    画面遷移エフェクトシーンをリファクタリング

--- root/autoload/game_manager/transition_effect/transition_effect.gd
+++ root/autoload/game_manager/transition_effect/transition_effect.gd
@@ -11,12 +11,12 @@ signal finished_fade_out
 func _ready() -> void:
 	Log.info("_ready TransitionEffect")
 	# 初期状態では非表示に設定
-	visible = false
+	t_effect.visible = false
 	
 func fade_in() -> void:
 	Log.debug("func fade_in")
 	# 遷移画面の表示を有効化
-	visible = true
+	t_effect.visible = true
 	# 遷移エフェクトを全て表示
 	t_effect.factor = 1.0
 	# 遷移エフェクトをアニメーションさせてフェードインを表現
@@ -24,13 +24,13 @@ func fade_in() -> void:
 	tween.tween_property(t_effect, "factor", 0.0, transition_time)
 	# 画面遷移終了時、画面表示を無効化
 	tween.finished.connect(func() -> void:
-		visible = false
+		t_effect.visible = false
 	)
 
 func fade_out() -> void:
 	Log.debug("func fade_out")
 	# 遷移画面の表示を有効化
-	visible = true
+	t_effect.visible = true
 	# 遷移エフェクトを非表示に設定
 	t_effect.factor = 0.0
 	# 遷移エフェクトをアニメーションさせてフェードアウトを表現
@@ -38,7 +38,7 @@ func fade_out() -> void:
 	tween.tween_property(t_effect, "factor", 1.0, transition_time)
 	# 画面遷移終了時、画面表示を無効化
 	tween.finished.connect(func() -> void:
-		visible = false
+		t_effect.visible = false
 		# フェードアウト終了のシグナルを発信
 		emit_signal("finished_fade_out")
 	)
